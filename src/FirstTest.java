981ea26f70c311e29dc9a9279b37a36751211a3c
aleksandraegorova <alesandraegorova8@gmail.com>
2026-01-08 15:09:00 +0300

    Ex5: Тест: Сохранение двух статей либо Онбординг

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -404,6 +404,137 @@ public class FirstTest {
 
     }
 
+    @Test
+    public void saveTwoArticlesToMyListDz()
+    {
+        WebElement element_skip = driver.findElementById("org.wikipedia:id/fragment_onboarding_skip_button");
+        element_skip.click();
+
+        waitForElementAndClick(By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5);
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Object-oriented programming language')]"),
+                "Cannot find 'Object-oriented programming language' searching by 'Java'",
+                15);
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Java (programming language)')]"),
+                "Cannot find article Java title",
+                15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/page_save"),
+                "Cannot find save button",
+                15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/snackbar_action"),
+                "Cannot find 'Add to list' button",
+                15);
+
+        String name_of_folder = "Learning programming";
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot find field 'Name'",
+                15);
+
+        waitForElementAndClick(By.id("android:id/button1"),
+                "Cannot find 'OK' button",
+                15);
+
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Cannot find back button",
+                5);
+
+        waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field to clear it",
+                5
+        );
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"),
+                "Python",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                "Cannot find 'Python (programming language)' searching by 'Python'",
+                15);
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                "Cannot find article Python title",
+                15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/page_save"),
+                "Cannot find save button",
+                15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/snackbar_action"),
+                "Cannot find 'Add to list' button",
+                15);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/item_title"),
+                "Cannot find existing list button",
+                15);
+
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Cannot find back button",
+                5);
+
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"),
+                "Cannot find back button",
+                5);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/nav_tab_reading_lists"),
+                "Cannot find 'Saved' button",
+                15);
+
+        waitForElementAndClick(By.xpath("//*[contains(@text, '" + name_of_folder + "')]"),
+                "Cannot find 'Learning programming' saved articles",
+                15);
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Object-oriented programming language')]"),
+                "Cannot find 'Object-oriented programming language'",
+                15);
+
+        waitForElementPresent(By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                "Cannot find 'Python (programming language)'",
+                15);
+
+        swipeElementToLeft(By.xpath("//*[contains(@text, 'Object-oriented programming language')]"),
+                "Cannot find 'Object-oriented programming language' for swiping to left");
+
+
+        waitForElementNotPresent(By.xpath("//*[contains(@text, 'Object-oriented programming language')]"),
+                "Cannot delete saved article",
+                15);
+
+        String title_in_saved_articles = waitForElementAndGetAttribute
+                (By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                        "text",
+                        "Cannot find title of article by 'Python (programming language)'",
+                        15);
+
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                "Cannot find 'Python (programming language)' to click",
+                15);
+
+        String title_of_saved_article = waitForElementAndGetAttribute
+                (By.xpath("//*[contains(@text, 'Python (programming language)')]"),
+                        "text",
+                        "Cannot find title of article by 'Python (programming language)'",
+                        15);
+
+        Assert.assertEquals("Article titles does not equal each other",
+                title_in_saved_articles,
+                title_of_saved_article);
+
+
+    }
+
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
     {
